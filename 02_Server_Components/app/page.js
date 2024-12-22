29a8e3f58dfd8fa6cd913e49b400cce4a14be985
vgarg1603 <145778938+vgarg1603@users.noreply.github.com>
2024-12-22 19:51:08 +0530

    Add files via upload

---
+++ 02_Server_Components/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container my-5">
+      <Image width={500} height={500} className="mx-auto " src="https://wowslider.com/sliders/demo-18/data1/images/hongkong1081704.jpg" alt="" />
+    </div>
+  );
+}
