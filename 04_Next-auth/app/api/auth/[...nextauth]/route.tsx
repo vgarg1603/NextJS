a8a5a023857a0b4c5cc5205402c1878d5d8158a3
vgarg1603 <145778938+vgarg1603@users.noreply.github.com>
2024-12-22 20:05:09 +0530

    Update route.tsx

--- 04_Next-auth/app/api/auth/[...nextauth]/route.tsx
+++ 04_Next-auth/app/api/auth/[...nextauth]/route.tsx
@@ -1,1 +1,14 @@
+import NextAuth from 'next-auth'
+// import GitHubProvider from "next-auth/providers/github";
 
+const handler = NextAuth({
+    providers: [
+        // OAuth authentication providers...
+        GitHubProvider({
+            clientId: example.env.ID,
+            clientSecret: example.env.SECRET
+        })
+    ]
+})
+
+export {handler as GET, handler as POST}
