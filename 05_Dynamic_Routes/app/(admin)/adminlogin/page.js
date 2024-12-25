9ae73b721aab92c308ad8fc187245fe6be95bf5c
vgarg1603 <145778938+vgarg1603@users.noreply.github.com>
2024-12-25 20:26:09 +0530

    Add files via upload

---
+++ 05_Dynamic_Routes/app/(admin)/adminlogin/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      this is login page
+    </div>
+  );
+}
+
+export default page;
---
+++ 05_Dynamic_Routes/app/(admin)/adminlogout/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      This is logout page
+    </div>
+  );
+}
+
+export default page;
